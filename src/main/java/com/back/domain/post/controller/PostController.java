cd2a7350067cba31e6cf86b351e3444643d67461
ppinggu9 <pping5644@gmail.com>
2026-03-05 14:34:00 +0900

    @NoBlank, @Size

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -2,6 +2,8 @@ package com.back.domain.post.controller;
 
 import com.back.domain.post.entity.Post;
 import com.back.domain.post.service.PostService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,25 +19,22 @@ public class PostController {
     @ResponseBody
     public String writForm() {
 
-        return getWriteForm("", "", "");
+        return getWriteForm("", "", "", "");
     }
 
     @PostMapping("/write")
     @ResponseBody
-    public String write(@RequestParam String title, String content) {
-        if (title.isBlank()) {
-            return """
-                    <div style="color:red">제목을 입력해주세요.</div>
-                    %s
-                    """.formatted(getWriteForm(title, content,"title"));
-        }
+    public String write(
+        @NotBlank @Size(min=2, max=10) String title,
+        @NotBlank @Size(min=2, max=100) String content
+    ) {
 
-        if (content.isBlank()) {
-            return """
-                    <div style="color:red">내용을 입력해주세요.</div>
-                    %s
-                    """.formatted(getWriteForm(title, content,"content"));
-        }
+        if(title.isBlank()) return getWriteForm("제목을 입력해주세요.", title, content, "title");
+        if(title.length() < 2) return getWriteForm("제목은 2글자 이상 적어주세요.", title, content, "title");
+        if(title.length() > 10) return getWriteForm("제목은 10글자 이상 넘을 수 없습니다.", title, content, "title");
+        if(content.isBlank()) return getWriteForm("내용을 입력해주세요.", title, content, "content");
+        if(content.length() < 2) return getWriteForm("내용은 2글자 이상 적어주세요.", title, content, "content");
+        if(content.length() > 100) return getWriteForm("내용은 100글자 이상 넘을 수 없습니다.", title, content, "content");
 
         Post post = postService.write(title, content);
 
@@ -43,8 +42,9 @@ public class PostController {
     }
 
 
-    private String getWriteForm(String title, String content, String errorFieldName) {
+    private String getWriteForm(String errorMessage, String title, String content, String errorFieldName) {
         return """
+                 <div style="color:red">%s</div>
                 <form method="POST" action="/posts/write">
                   <input type="text" name="title" value="%s" autoFocus>
                   <br>
@@ -60,6 +60,6 @@ public class PostController {
                         form[errorFieldName].focus();
                     }
                 </script>
-                """.formatted(title, content, errorFieldName);
+                """.formatted(errorMessage,title, content, errorFieldName);
     }
 }
