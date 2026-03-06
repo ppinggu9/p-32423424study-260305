eed14a8fbcc4bda9ae45c528f2abef98d0554e10
ppinggu9 <pping5644@gmail.com>
2026-03-06 12:42:11 +0900

    ModelAttribute()의 작동방식 + form 설계상 구조를 고려한 리팩토링

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -6,13 +6,15 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.stream.Collectors;
 
@@ -24,12 +26,12 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/write-form")
-    @ResponseBody
-    public String writeForm() {
-        return getWriteForm("", "", "");
+    public String writeForm(@ModelAttribute("form") WriteRequestForm form) {
+        return "write";
     }
 
     @AllArgsConstructor
+    @Getter
     public static class WriteRequestForm {
         @Size(min = 2, max = 10, message = "3-제목은 2자 이상 10자 이하로 입력해주세요.")
         @NotBlank(message = "1-제목은 필수입니다.")
@@ -41,8 +43,7 @@ public class PostController {
     }
 
     @PostMapping("/write")
-    @ResponseBody
-    public String write(@Valid WriteRequestForm form, BindingResult bindingResult) {
+    public String write(@Valid @ModelAttribute("form") WriteRequestForm form, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
             String errorMessages = bindingResult.getFieldErrors()
@@ -55,35 +56,15 @@ public class PostController {
                     .sorted()
                     .collect(Collectors.joining("\n"));
 
-            return getWriteForm(errorMessages, form.title, form.content);
+            // 템플릿 응답
+            model.addAttribute("errorMessages", errorMessages);
+            return "write";
         }
 
         Post post = postService.write(form.title, form.content);
 
-        return "%d번 글이 작성되었습니다.".formatted(post.getId());
-    }
-
-    private String getWriteForm(String errorMessage, String title, String content) {
-        return """
-                <ul style="color:red">
-                %s
-                </ul>
-                <form method="POST" action="/posts/write">
-                  <input type="text" name="title" value="%s" autoFocus>
-                  <br>
-                  <textarea name="content">%s</textarea>
-                  <br>
-                  <input type="submit" value="작성">
-                </form>
-                <script>
-                    const li = document.querySelector("ul li");
-                    const errorFieldName = li.dataset.errorField;
-                
-                    if(errorFieldName.length > 0) {
-                        const form = document.querySelector("form");
-                        form[errorFieldName].focus();
-                    }
-                </script>
-                """.formatted(errorMessage, title, content);
+        // 템플릿 응답
+        model.addAttribute("id", post.getId());
+        return "writeDone";
     }
 }
